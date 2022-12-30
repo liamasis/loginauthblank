8da836c70b156369a5f6d998b7e22560a9b081b9
liamasis <liamcalebob@gmail.com>
2022-12-30 16:22:31 +1300

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="constainer">
+      <form action="">
+        <input type="text" />
+        <input type="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+export default Login;
