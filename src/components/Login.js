1f069dadfa5873dc19804ec9b9d0c5dc02c820ae
liamastray <liamcalebob@gmail.com>
2023-01-19 13:29:14 +1100

    day 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios"
 
 const Login = () => {
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user,setUser] = useState({})
+  const clickChange = async (e) = {
+    e.preventDefault()
+    
+  }
+  
   return (
     <div className="constainer">
       <form action="">
