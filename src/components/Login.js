5567c45293605e40c2688ab993cc082fa75076fc
liamastray <liamcalebob@gmail.com>
2023-01-19 20:53:48 +1100

    fuck

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,14 @@ import React, { useState } from "react";
 
 
 const Login = () => {
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState({});
 
   return (
     <div className="constainer">
       <form action="">
         <h1>username</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" />
-        <button>Login</button>
+        <input type="text" placeholder="username" value="" />
+        <input type="password" placeholder="password" />
+        <button disabled="true">Login</button>
       </form>
     </div>
   );
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,7 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+
 test("username input shoudld be rendered", () => {
   render(<Login />);
   const userInputEl = screen.getByPlaceholderText(/username/i);
   expect(userInputEl).toBeInTheDocument();
+});
+
+test("password input shoudld be rendered", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  expect(passwordInput).toBeInTheDocument();
+});
+
+test("username input shoudld be rendered", () => {
+  render(<Login />);
+  const buttonInputEl = screen.getByRole("button");
+  expect(buttonInputEl).toBeInTheDocument();
+});
+
+test("username input shoudld be empty", () => {
+  render(<Login />);
+  const buttonInputEl = screen.getByPlaceholderText(/username/i);
+  expect(buttonInputEl.value).toBe("");
+});
+
+test("username input shoudld be rendered", () => {
+  render(<Login />);
+  const buttonInputEl = screen.getByRole("button");
+  expect(buttonInputEl).toBeDisabled();
 });
