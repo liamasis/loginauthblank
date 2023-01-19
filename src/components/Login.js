bafd84260c86afe4e11e87cd800197636bb2aeda
liamastray <liamcalebob@gmail.com>
2023-01-19 14:35:51 +1100

    just restarted vscode, seemed to fix emmet

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,18 @@
-import React, {useState} from "react";
-import axios from "axios"
+import React, { useState } from "react";
+
 
 const Login = () => {
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [user,setUser] = useState({})
-  const clickChange = async (e) = {
-    e.preventDefault()
-    
-  }
-  
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
   return (
     <div className="constainer">
       <form action="">
-        <input type="text" />
+        <h1>username</h1>
+        <input type="text" placeholder="username" />
         <input type="password" />
         <button>Login</button>
       </form>
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 test("username input shoudld be rendered", () => {
-    render(<Login />);
-    const userInputEl = screen.getByPlaceholderText(/username/i);
-
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i);
+  expect(userInputEl).toBeInTheDocument();
 });
