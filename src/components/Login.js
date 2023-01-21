ebcc633dae595991bd902e64878905138ea4cca7
liamasis <liamcalebob@gmail.com>
2023-01-22 10:52:05 +1100

    new day new dollar

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="constainer">
       <form action="">
-        <input type="text" />
-        <input type="password" />
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
         <button>Login</button>
       </form>
     </div>
