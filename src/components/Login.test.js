d92cf6d0998900500a3bd6141664c2f0f8fc5a40
liamasis <liamcalebob@gmail.com>
2023-01-22 15:13:00 +1100

    6 tests

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Login from "./Login";
 test("check if username field has rendered", () => {
   render(<Login />);
@@ -22,4 +22,21 @@ test("check if error message not there on load", () => {
   render(<Login />);
   const errormsg = screen.getByTestId("errormsg");
   expect(errormsg).not.toBeVisible();
+});
+
+test("username should change", () => {
+  render(<Login />);
+  const usernameField = screen.getByPlaceholderText(/username/i);
+  const test = "test";
+  fireEvent.change(usernameField, { target: { value: test } });
+
+  expect(usernameField.value).toBe(test);
+});
+
+test("check if password field has rendered", () => {
+  render(<Login />);
+  const passwordField = screen.getByPlaceholderText(/password/i);
+  const test = "test";
+  fireEvent.change(passwordField, { target: { value: test } });
+  expect(passwordField.value).toBe(test);
 });
