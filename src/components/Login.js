33ee1cc7ba28f3f827da31b70595a4efb8243398
liamasis <liamcalebob@gmail.com>
2023-01-22 11:04:02 +1100

    button functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,28 @@ import { useState } from "react";
 import React from "react";
 
 const Login = () => {
+  const [error, setError] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <div className="constainer">
       <form action="">
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button diasbled={!password || !username}>Login</button>
       </form>
+      <span></span>
     </div>
   );
 };
