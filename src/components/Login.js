1712af7262af4a43024c0552065b1dfdfc36c543
liamasis <liamcalebob@gmail.com>
2023-01-22 11:23:11 +1100

    not tests new functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,21 @@ const Login = () => {
   const [error, setError] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      setUsername(data);
+    } catch {
+      setError(true);
+    }
+    setLoading(false);
+  };
   return (
     <div className="constainer">
       <form action="">
@@ -21,9 +35,16 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button diasbled={!password || !username}>Login</button>
+        <button disabled={!password || !username} onClick={handleClick}>
+          {loading ? "Loading" : "Login"}
+        </button>
       </form>
-      <span></span>
+      <span
+        testdata-id="errormsg"
+        style={{ visibility: error ? "false" : "true" }}
+      >
+        There has been an error
+      </span>
     </div>
   );
 };
