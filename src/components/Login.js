cc0e2eb7e32655144164715ffae61cb12d74fe51
liamastray <liamcalebob@gmail.com>
2023-01-24 20:52:47 +1100

    fuck

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { useState } from "react";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
   return (
     <div className="constainer">
       <form action="">
-        <input type="text" />
-        <input type="password" />
-        <button>Login</button>
+        <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button disabled={!password || !username}>Login</button>
       </form>
     </div>
   );
