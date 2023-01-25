bf7be5562bcf043a54841d91c744d811b299493f
liamasis <liamcalebob@gmail.com>
2023-01-25 16:28:35 +1100

    new commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,26 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      setUser(data);
+    } catch {}
+  };
   return (
     <div className="constainer">
+      <h1>{user ? user.name : ""}</h1>
       <form action="">
         <input
           type="text"
@@ -19,7 +34,9 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button disabled={!password || !username}>Login</button>
+        <button disabled={!password || !username} onClick={handleClick}>
+          {loading ? "Loading" : "Login"}
+        </button>
         <span style={{ visibility: error ? "visible" : "hidden" }}>
           There has been a mistake
         </span>
